6efb130959ac078914b225024d8052edd35bd3ba
Patricia Castellanos <patricia.castellanos@nasa.gov>
2023-04-10 18:15:52 -0400

    PC - implement getslice and setslice within getitem and setitem

--- src/pyobs/bits.py
+++ src/pyobs/bits.py
@@ -10,23 +10,29 @@ class BITS(object):
         self._d = value
 
     def __getitem__(self, index):
-        return (self._d >> index) & 1 
+        if isinstance(index, slice):
+            start = index.start
+            end   = index.stop
+            mask = 2**(end - start) -1
+            return (self._d >> start) & mask
+        elif isinstance(index, int):
+            return (self._d >> index) & 1 
 
     def __setitem__(self,index,value):
-        value    = (value&1)<<index
-        mask     = (1)<<index
-        self._d  = (self._d & ~mask) | value
+        if isinstance(index, slice):
+            start = index.start
+            end   = index.end
+            mask = 2**(end - start) -1
+            value = (value & mask) << start
+            mask = mask << start
+            self._d = (self._d & ~mask) | value
+            return (self._d >> start) & mask
+        elif isinstance(index, int):
+            value    = (value&1)<<index
+            mask     = (1)<<index
+            self._d  = (self._d & ~mask) | value
 
-    def __getslice__(self, start, end):
-        mask = 2**(end - start) -1
-        return (self._d >> start) & mask
 
-    def __setslice__(self, start, end, value):
-        mask = 2**(end - start) -1
-        value = (value & mask) << start
-        mask = mask << start
-        self._d = (self._d & ~mask) | value
-        return (self._d >> start) & mask
 
     def __int__(self):
         return self._d
