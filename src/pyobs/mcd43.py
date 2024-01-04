3d92c13bc84c35b9dabaffa782c9c6bfeabf4901
Arlindo da Silva <arlindo.m.dasilva@nasa.gov>
2024-01-04 09:52:42 -0500

    ams: work in progress

--- src/pyobs/mcd43.py
+++ src/pyobs/mcd43.py
@@ -5,12 +5,13 @@ Reads Level 3 grid MXD43 BRDF files.
 
 import os
 import sys
-from numpy    import loadtxt, array, tile, where, concatenate
-from numpy import ones
+import numpy       as np
+import cartopy.crs as ccrs
+import xarray      as xr
+
 from datetime import date, datetime, timedelta
 from glob     import glob
-from pyhdf.SD import SD, HDF4Error
-from .bits import BITS
+#from pyhdf.SD import SD, HDF4Error
 
 MISSING = -99999
 
@@ -37,6 +38,63 @@ ALIAS = dict ( BRDF_Albedo_Parameters_Band1 = 'BRDF_b1_645',
 
 #...........................................................................
 
+modis  = ccrs.Sinusoidal.MODIS # Built in
+latlon = ccrs.PlateCarree()
+
+# 4 points West, East, South, North
+# ---------------------------------
+nH = 36
+nV = 18
+
+x_180, dummy = modis.transform_point(-180,0,latlon)   
+x180,  dummy = modis.transform_point(180,0,latlon)   
+dummy,  y_90 = modis.transform_point(0,-90,latlon)   
+dummy,  y90  = modis.transform_point(0,90,latlon)   
+       
+dH = (x180-x_180)/nH     # delta-h, size of each horizontal tile
+dV = (y90-y_90)/nV       # delta-v, size of each vertical tile
+
+    
+#...........................................................................
+  
+def _ll2sinu(lon,lat):
+    """
+    Given (lon,lat) coordinates, returns (x,y) coordinates of MODIS sinusoidal
+    transform. On input, (lon,lat) can be scalars or arrays.
+    """
+
+    # Input are arrays
+    # ----------------
+    if isinstance(lon,np.ndarray):
+        z = modis.transform_points(latlon, lon, lat)
+        return (z[:,0], z[:,1]) # x, y coordinartes
+
+    # Input are scalars
+    # -----------------
+    else:
+        return modis.transform_point(lon,lat, latlon)
+
+def _tn2bbox(TileName):
+    """
+    Given a tile name, return bounding boxes.
+    TileName example: h32v08
+    """
+    h = int(TileName[1:3])
+    v = nV - 1 - int(TileName[4:6]) # vertical tiles are upside down!
+
+    xs, ys = np.zeros(2), np.zeros(2)
+     
+    xs[0] = x_180 + h*dH
+    xs[1] = xs[0] + dH
+    
+    ys[0] = y_90  + v*dV
+    ys[1] = ys[0] + dV
+
+    return (xs,ys)
+    
+
+#...........................................................................
+
 class McD43(object):
     """
     This class implements the MODIS LAND BRDF daily Level 3 products, MCD43B1 (1000m horz res), 
@@ -46,185 +104,196 @@ class McD43(object):
        """
        Reads individual tile files for full day of Level 3 MCD43 
        present on a given *Path* and returns a objects with
-       all 3 kernels coeff. On input, 
+       all 3 kernels coeff.  
 
-       Required parameters:
+       On input,
          Path -- can be a single file, a single directory, of a list
                  of files and directories. 
          
-       Optional parameters:
-         res  --- horizontal resolution of the file
-                  by default : MCD43B files--> 1km res
-                  MCD43A files --> 500 m                     
+         lon, lat -- coordinates to sample BRDF kernels
+       
        """
 
-       # List of HDF files for a given date
-       #-----------------------------------
+       # List of HDF files for a given directory,
+       # typically corresponding to a date.
+       #-----------------------------------------
        self.verb = Verb
        self.SDS = SDS['LAND'] 
-       self.Tfiles = glob(Path + '*.hdf')
-      
-    
-       # From a list of lat and lon, return the tile numbers v(vertical), h(horiz),
-       # and dx, dy inside the tile
-       # -------------------------------------  
+
+       # Lazyload each MCD43 file, retrieving bounding boxes
+       # and assigning missing coordinate variables
+       # ---------------------------------------------------
+       self._lazyload(Path)
+
+       # From a list of lat and lon, return the tile numbers v(vertical), h(horiz)
+       # global coordinates (x,y) inside each tile
+       # -------------------------------------------------------------------------  
        self.nobs = len(lon)
-       self._findTile(Path,lon,lat)
+       self._findTile(lon,lat)
        
-       # Create empty lists for SDS to be read from file
-       # -----------------------------------------------
-#       for name in self.SDS:
-#           self.__dict__[name] = [] 
-
-       # Read BRDF kernel in a MODIS tile
-       # --------------------------------- 
-       self.read_BRDF()
+       # Read BRDF kernels at each observation location
+       # ----------------------------------------------
+       self.interpBRDF()
       
-       # Result
-       
+          
+#---
+    def _lazyload(self,Path):
+        """
+        Lazy load each tile file, retrieve bounding boxes
+        and add coordinate variables to xarray object.
+        """
+        TileFileNames = glob(Path + '*.hdf')
+        self.Tiles = dict()
+        for fn in TileFileNames:
+            tn = os.path.basename(fn).split('.')[2]
+            ds = xr.open_dataset(fn,engine='netcdf4')
             
-       
-#--- 
-    def _findTile(self,path,lon,lat):
+            ## xs, ys = np.zeros(2), np.zeros(2)
+            ## meta = ds.attrs['ArchiveMetadata.0']
+            ## lons = np.array((float(meta.split('WESTBOUNDINGCOORDINATE')[1].split('=')[2].split('\n')[0]),
+            ##                  float(meta.split('EASTBOUNDINGCOORDINATE')[1].split('=')[2].split('\n')[0]) ))
+            ## lats = np.array(( float(meta.split('SOUTHBOUNDINGCOORDINATE')[1].split('=')[2].split('\n')[0]),
+            ##                   float(meta.split('NORTHBOUNDINGCOORDINATE')[1].split('=')[2].split('\n')[0]) ))
+            
+            ## meta = ds.attrs['StructMetadata.0']
+            ## xs[0],ys[1] = np.array(meta.split('UpperLeftPointMtrs')[1].split('\n')[0].replace('=(','').replace(')','').split(',')).astype('float')
+            ## xs[1],ys[0] = np.array(meta.split('LowerRightMtrs')[1].split('\n')[0].replace('=(','').replace(')','').split(',')).astype('float')
 
-       """Given a list of lat, lon, return numbers to find the tile(v,h) 
-          and position inside the tile (dx,dy)
+            ## self.Tiles[tn] = dict ( ds=ds, lons=lons, lats=lats, xs=xs, ys=ys )
+
+            # Give dimensions sensible names
+            # ------------------------------
+            ds = ds.rename({'XDim:MOD_Grid_BRDF': 'x',
+                            'YDim:MOD_Grid_BRDF': 'y',
+                            'Num_Parameters:MOD_Grid_BRDF': 'k'})
+            self.Tiles[tn] = ds
+
+        # Add coordinate variables
+        # ------------------------
+        self.nx, self.ny = ds.dims['x'], ds.dims['y']
+        for tn in self.Tiles:
+            xs, ys = _tn2bbox(tn)
+            x = np.linspace(xs[0],xs[1],self.nx,endpoint=True)
+            y = np.linspace(ys[1],ys[0],self.ny,endpoint=True)  # Vertical gris is North to South
+            self.Tiles[tn] = ds.assign_coords({'x': x, 'y':y})  # dataset now has sinusoidal coordinates with sensible names
+            
+    def _findTile(self,lon,lat):
+
        """
-       from mpl_toolkits.basemap import Basemap # Basemap is deprecated, needs refatoring
-       m = Basemap(projection='sinu',lon_0=0,rsphere=6371007.181,resolution='c')
-       x,y=m(lon,lat)
+          Given a list of lat, lon, find tile coordinates (v,h) and the
+       unique set of tiles that contain those observations.
+       """
 
-       x_180,y_180 = m(-180,0)   # 4 points West, East, South, North
-       x180,y180 = m(180,0)
-       x_90,y_90 = m(0,-90)
-       x90,y90 = m(0,90)
+       self.lon, self.lat = lon, lat
+       x, y = _ll2sinu(lon,lat)  # corresponding sinusoidal coordinates
+
+       # Make these DataArrays with fake dimension
+       # -----------------------------------------
+       self.x = xr.DataArray(x, dims='nobs')
+       self.y = xr.DataArray(y, dims='nobs')
+
+       v  = nV - 1 - (self.y-y_90)//dV     # tile vertical coordinate v in [0,18]
+       h  = (self.x-x_180)//dH             # tile horizontal coordinate in [0,36]
+
+       self.obsTileIndex = dict()        # index of those observations on a given tile
+       T1D = v*nH + h
+       for t1d in np.unique(T1D):        # loop over unique set of tiles
+           v = t1d // nH
+           h = t1d % nH
+           tn = 'h%02dv%02d'%(h,v)            # tile name, e.g., h00v18
+           self.obsTileIndex[tn] = T1D==t1d   # save obs indices corresponding to this tile
        
-       dv = tile(int((y90-y_90)/18.),len(x))
-      
-       N = tile(18.,len(x))        # 18 vertical tiles in MODIS
-       self.v = N-(y/dv)           # return number v tile for a list of lat, lon
-
-       dh = tile(int((x180-x_180)/36.), len(x))
-       self.h = x/dh
-      
-                       
-       dirn, filen = os.path.split(self.Tfiles[0])   # verify dim of first file
-       tokens = filen.split('.')    
-       if tokens[0]=='MCD43B1' :
-          xdim = 1200.
-       else :
-          print("- %s:not MCD43B file--> check resolution"%tokens[0])
-       if self.verb:
-          print('xdim', xdim)
-       xdim_ = tile(xdim,len(x))
-
-       int_h = [int(i) for i in self.h]
-       int_v = [int(i) for i in self.v]
-      
-       dx = (self.h-int_h)*xdim_
-       dy = (self.v-int_v)*xdim_
-
-       self.dx = [int(i) for i in dx]
-       self.dy = [int(i) for i in dy]
-  
-  
-       self.h = int_h               # keep only real part
-       self.v = int_v
-       if self.verb:
-          print('dx','dy', self.dx,self.dy)
-      
-       # create a list of tiles name associated with each (lat, lon) and (h,v)
-       # -------------------
-       self.Tiles = [] 
-       for f in self.Tfiles :
-                 dirn, filen = os.path.split(f)
-                 tokens = filen.split('.')
-                 for i in range(len(self.h)):
-                     if (self.h[i] == int(tokens[2][1:3])) and (self.v[i] == int(tokens[2][4:6])) :
-                         self.Tiles.append(f)
-
-       # create a unique list of files
-       # ----------------
-       uniq = dict()
-       for fn in array(self.Tiles) :
-           uniq[fn] = 0 
-#       self.unique_fn = uniq.keys()
-
-       self.unique_fn = []
-       # Get the index I for each file
-       # ----------------------------
-       for fn in list(uniq.keys()) :
-           I = [array(self.Tiles) == fn]
-           self.unique_fn.append((fn, I))
-
 #---
-    def get_Var(self, Vname = None):
+    def interpBRDF(self):
+        """
+        Sample BRDF kernels from MCD43A1/B1 at observation locations.
+        """
 
-        """Given filename dx, dy for a tile, return the 3 kernels for BRDF"""
-        nch = 7
-        BRDF = MISSING * ones((self.nobs,nch)) 
-        for fn,I in self.unique_fn:
-              self._read_BRDF(fn,I[0])
-              if self.verb:
-                print(I)
-              BRDF
-#---
-    def read_BRDF(self):
-       """Reads MCD43B1 one Tile file with Level 3 BRDF kernels for each MODIS band."""
+        for sds in self.SDS:
+           self.__dict__[sds] = xr.DataArray(np.ones((self.nobs,3)),
+                                             coords = { 'lon': ('nobs',self.lon),
+                                                        'lat': ('nobs',self.lat)},
+                                             dims=('nobs','k'))
+           #print(sds,self.__dict__[sds])
+    
+        for tn in self.obsTileIndex:
 
-       # Create empty lists for SDS to be read from file
-       # -----------------------------------------------
-       for name in self.SDS:
-           self.__dict__[name] = [] 
+           I  = self.obsTileIndex[tn]  # obs indices on this tile
+           ds = self.Tiles[tn]         # xarray corresponding to tile
+           
+           x, y = self.x[I], self.y[I] # obs coords on this tile
 
-       BRDF = MISSING * ones((len(self.SDS),self.nobs,3)) 
+           print(tn,'lon = ',self.lon[I], 'lat = ',self.lat[I])
+           
+           for sds in self.SDS:
+               self.__dict__[sds][I,:] = ds[sds].sel(x=x, y=y,method='nearest').values
 
-       for fn, I in self.unique_fn:
-         index = I[0]
-         if self.verb:
-            print(index, type(index), len(index)) 
-          # Don't fuss if the file cannot be opened
-          # ---------------------------------------
-         try:
-            if self.verb:
-                print("[] Working on "+fn)
-            hfile = SD(fn)
-         except HDF4Error:
-            if self.verb > 2:
-                print("- %s: not recognized as an HDF file"%filename)
-            return 
-
-          # Read select variables (reshape to allow concatenation later)
-          # ------------------------------------------------------------
-         for sds in self.SDS:  
-            if self.verb:
-              print('sds',self.SDS.index(sds))                 
-            v = hfile.select(sds).get()           
-            a = hfile.select(sds).attributes()
-            if a['scale_factor']!=1.0 or a['add_offset']!=0.0:
-                v = a['scale_factor'] * v + a['add_offset']
-            if self.verb:
-              print(array(self.dx)[index], BRDF.shape, BRDF[self.SDS.index(sds),index], v.shape) 
-
-            BRDF[self.SDS.index(sds),index,:] = v[array(self.dx)[index], array(self.dy)[index], :]
-
-       for sds in self.SDS:  
-           self.__dict__[sds] = BRDF[self.SDS.index(sds),:,:]  
+        # Convenient aliases
+        for sds in self.SDS:
+           #print(sds,self.__dict__[sds])
            if sds in list(ALIAS.keys()):
                self.__dict__[ALIAS[sds]] = self.__dict__[sds] 
          
-        
-#---
 
 #............................................................................
 
+def fluxnet(filen):
+
+    import pandas as pd
+    
+    flx = xr.open_dataset(filen)
+
+    Lon  = flx['Longitude']
+    Lat  = flx['Latitude']
+    Name = flx['Name']
+
+    Stations = np.unique(Name)
+
+    lons, lats = [], []
+    for stn in Stations:
+
+        I = Name==stn
+
+        lons += [Lon[I][0],]
+        lats += [Lat[I][0],]
+
+    variables = dict( lons=np.array(lons), lats=np.array(lats) )
+    return pd.DataFrame( variables,index=Stations)     
+    
+
 if __name__ == "__main__":
 
+      fluxnet_fn = '/Users/adasilva/data/brdf/onefluxnet_daily_mcd43_c61.nc'
+      mcd43a1_dn = '/Users/adasilva/data/brdf/2023/204/'
 
-#      path = '/nobackup/2/vbuchard/MODIS_LAND/MCD43B1/20070626/MCD43B1.A2007177.h%h2v%v2.*'
-      path = '/nobackup/2/vbuchard/MODIS_LAND/MCD43B1/20070626/'
+      stations = fluxnet(fluxnet_fn)
+
+      print(stations)
+
+      lon = stations['lons'].values
+      lat = stations['lats'].values
       
-      lon=[-2.,-120.,15.2,17.2,170.1]
-      lat=[88.,40.,-20.,-20.,-55.5]
-      ex = McD43(path,lon,lat)
+      brdf = McD43(mcd43a1_dn,lon,lat)
 
+
+def xxx():
+      
+      for tn in brdf.Tiles:
+            ds = brdf.Tiles[tn]
+            xs, ys = _tn2bbox(tn)
+            x, y = ds['x'], ds['y']
+            xs = np.array((x.min(), x.max()))
+            ys = np.array((y.min(), y.max()))
+            lons, lats = np.zeros(2), np.zeros(2)
+            lons[0],lats[0] = latlon.transform_point(xs[0],ys[0],modis)
+            lons[1],lats[1] = latlon.transform_point(xs[1],ys[1],modis)
+            print(tn)
+            print('lons = ',lons, 'lats = ',lats)
+            #print('  xs = %20.3f %20.3f %20.3f'%(tile['xs'][0],tile['xs'][1],tile['xs'][1]-tile['xs'][0]))
+            print('  XS = %20.3f %20.3f %20.3f'%(xs[0],xs[1],xs[1]-xs[0]))
+            print('  YS = %20.3f %20.3f %20.3f'%(ys[0],ys[1],ys[1]-ys[0]))
+
+      print()
+      print('x limits = ',x_180,x180,dH)
+      print('y limits = ',y_90,y90,dV)
+      
