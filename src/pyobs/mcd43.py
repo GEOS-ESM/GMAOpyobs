515f4dbc8f9b2a22f4719f2dda4812c29a204569
Arlindo da Silva <arlindo.m.dasilva@nasa.gov>
2024-01-06 18:58:23 -0500

    ams: polished API, added options for returning dataframes

--- src/pyobs/mcd43.py
+++ src/pyobs/mcd43.py
@@ -4,13 +4,11 @@ Reads Level 3 grid MXD43 BRDF files.
 """
 
 import os
-import sys
 import numpy       as np
 import cartopy.crs as ccrs
 import xarray      as xr
-
-from warnings import warn
-from datetime import date, datetime, timedelta
+import pandas      as pd
+    
 from glob     import glob
 
 #...........................................................................
@@ -104,9 +102,7 @@ class McD43(object):
        all 3 kernels coeff.  
 
        On input,
-         Path : can be a single file, a single directory, of a list
-                 of files and directories. 
-         
+         Path : directory where to find tile files for a given day         
          lon, lat : coordinates to sample variables on
          addLatLon : whether to add 2D lat/lon coordinates to tiles;
                      this is only necessary for testing interpolation,
@@ -170,10 +166,14 @@ class McD43(object):
             
             # Give dimensions sensible names
             # ------------------------------
-            ds = ds.rename({'XDim:MOD_Grid_BRDF': 'x',
-                            'YDim:MOD_Grid_BRDF': 'y',
-                            'Num_Parameters:MOD_Grid_BRDF': 'k'})
-
+            if 'Num_Parameters:MOD_Grid_BRDF' in ds.dims:
+                ds = ds.rename({'XDim:MOD_Grid_BRDF': 'x',
+                                'YDim:MOD_Grid_BRDF': 'y',
+                                'Num_Parameters:MOD_Grid_BRDF': 'k'})
+            else:
+                ds = ds.rename({'XDim:MOD_Grid_BRDF': 'x',
+                                'YDim:MOD_Grid_BRDF': 'y'})
+ 
             self.Tiles[tn] = ds
 
         # Add coordinate variables
@@ -184,18 +184,26 @@ class McD43(object):
             x = np.linspace(xs[0],xs[1],self.nx,endpoint=True)
             y = np.linspace(ys[1],ys[0],self.ny,endpoint=True)  # Vertical gris is North to South
 
+            ds = self.Tiles[tn]
+            
             if addLatLon:
                 X, Y = np.meshgrid(x,y)
                 Lon, Lat = _sinu2ll(X.flatten(),Y.flatten())
                 Lon = xr.DataArray(Lon.reshape(X.shape),dims=('x','y'))
                 Lat = xr.DataArray(Lat.reshape(Y.shape),dims=('x','y'))
 
-                self.Tiles[tn] = ds.assign_coords({  'x': x,    'y':y,    # dataset now has sinusoidal coordinates with sensible names
+                self.Tiles[tn] = ds.assign_coords({  'x': x,    'y':y,    # sinusoidal coords
                                                    'lon':Lon, 'lat':Lat}) # and 2D lat/lon coordinates
                                                    
             else:
-                self.Tiles[tn] = ds.assign_coords({'x': x, 'y':y})  # dataset now has sinusoidal coordinates with sensible names
-            
+                self.Tiles[tn] = ds.assign_coords({'x': x, 'y':y})        # sinusoidal coords
+
+        # Convenience list of variables, coordinates and dimensions
+        # ---------------------------------------------------------
+        self.coords, self.variables, self.dims = list(ds.coords), list(ds.variables), list(ds.dims)
+        for v in self.variables:
+            if v in self.coords:
+                self.variables.remove(v) # exclude coordinate variables
 #---
     def interp(self, vname, method='nearest'):
         """
@@ -229,15 +237,15 @@ class McD43(object):
                    raise ValueError('x out of range for '+tn)
                if y.min()<Y.min() or y.max()>Y.max():
                    raise ValueError('y out of range for '+tn)
-           
+
            if method == 'nearest':
-               vinterp = ds[vname].sel(x=x, y=y,method=method).values
+               vinterp = ds[vname].sel(x=x, y=y,method=method)
            else:               
-               vinterp = ds[vname].interp(x=x, y=y,method=method).values
+               vinterp = ds[vname].interp(x=x, y=y,method=method)
 
            if len(vinterp.shape) == 1:
-               var[I,0] = vinterp          # typically for lon/lat (debugging only)
-           elif len(vinterp.shape) == 2: 
+               var[I,0] = vinterp          
+           elif len(vinterp.shape) == 2:   
                var[I,:] = vinterp
            else:
                raise Warning('Internal error, this should never happen!')
@@ -248,11 +256,33 @@ class McD43(object):
            return var
 
 #---
+    def interp_many(self,Variables=None,Index=None):
+        """
+        Sample all variables on file, returning an xarray Data DataFrame with all
+        interpolated variables. On input,
+
+        Variables : list of variables to interpolate (default: all variables)
+        Index : Pandas DataFrame index; if not specified, an Xarray dataset is returned.
+      
+        """
+        if Variables is None:
+            Variables = self.variables
+        variables = dict()
+        if Index is not None:       
+            variables['lon'], variables['lat'] = self.lon, self.lat
+        for vname in Variables:
+            variables[vname] = self.interp(vname)
+     
+        if Index is None:       
+            return xr.Dataset(variables)
+        else:
+            return pd.DataFrame(variables,index=Index)
+#---
     def _getBoundsFromMetatada(self,ds):
         """
         Given a MCD43 granule xarray dataset, returns bounds
         found in the metadata. Useful to verify correctness of
-        _tn2bbox().
+        _tn2bbox(), not really used otherwise.
         """
 
         meta = ds.attrs['ArchiveMetadata.0']
@@ -270,65 +300,35 @@ class McD43(object):
 
 #............................................................................
 
-def fluxnet(filen):
-
-    import pandas as pd
-    
-    flx = xr.open_dataset(filen)
-
-    Lon  = flx['Longitude']
-    Lat  = flx['Latitude']
-    Name = flx['Name']
-
-    Stations = np.unique(Name)
-
-    lons, lats = [], []
-    for stn in Stations:
-
-        I = Name==stn
-
-        lons += [Lon[I][0],]
-        lats += [Lat[I][0],]
-
-    variables = dict( lons=np.array(lons), lats=np.array(lats) )
-    return pd.DataFrame(variables,index=Stations)     
-    
-
 if __name__ == "__main__":
 
-      __DEBUG__ = True
-    
-      fluxnet_fn = '/Users/adasilva/data/brdf/onefluxnet_daily_mcd43_c61.nc'
-      mcd43a1_dn = '/Users/adasilva/data/brdf/2023/204/'
+      fluxnet_fn = '/Users/adasilva/data/brdf/fluxnet_stations.csv'
+      mcd43a1_dn = '/Users/adasilva/data/brdf/MCD43A4/2023/204/'
 
-      stations = fluxnet(fluxnet_fn)
+      stations = pd.read_csv('/Users/adasilva/data/brdf/fluxnet_stations.csv',
+                             index_col=0)
 
       print(stations)
 
       lon = stations['lons'].values
       lat = stations['lats'].values
-      
-      brdf = McD43(mcd43a1_dn,lon,lat,addLatLon=False)
+
+      # BRDF object properly initialized
+      # --------------------------------
+      brdf = McD43(mcd43a1_dn,lon,lat)
+
+      # Sample 1 variable at a time at obs locations
+      # --------------------------------------------
+      for vname in brdf.variables:
+            print('[] Interpolating',vname)
+            v = brdf.interp(vname)
+
+      # Alternativaly, interpolate several variables
+      # Note: omit variables to interpolate all variables
+      # -------------------------------------------------
+      Variables = ['Nadir_Reflectance_Band1', 'Nadir_Reflectance_Band2']
+      V = brdf.interp_many(Variables,Index=stations.index)
+      print('\n Dataframe:')
+      print(V)
 
 
-def xxx():
-      
-      for tn in brdf.Tiles:
-            ds = brdf.Tiles[tn]
-            xs, ys = _tn2bbox(tn)
-            x, y = ds['x'], ds['y']
-            xs = np.array((x.min(), x.max()))
-            ys = np.array((y.min(), y.max()))
-            lons, lats = np.zeros(2), np.zeros(2)
-            lons[0],lats[0] = latlon.transform_point(xs[0],ys[0],modis)
-            lons[1],lats[1] = latlon.transform_point(xs[1],ys[1],modis)
-            print(tn)
-            print('lons = ',lons, 'lats = ',lats)
-            #print('  xs = %20.3f %20.3f %20.3f'%(tile['xs'][0],tile['xs'][1],tile['xs'][1]-tile['xs'][0]))
-            print('  XS = %20.3f %20.3f %20.3f'%(xs[0],xs[1],xs[1]-xs[0]))
-            print('  YS = %20.3f %20.3f %20.3f'%(ys[0],ys[1],ys[1]-ys[0]))
-
-      print()
-      print('x limits = ',x_180,x180,dH)
-      print('y limits = ',y_90,y90,dV)
-      
