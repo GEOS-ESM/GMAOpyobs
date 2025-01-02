35e08541a2765a4c3b554ac6eabaabf1c0805c29
Santiago Gasso <sgasso@discover11.prv.cube>
2025-01-02 17:11:54 -0500

    Updated calipso reading functionaly so it can be used by triple collocation code

---
+++ calipso_l2.py
@@ -0,0 +1,458 @@
+#!/bin/env python
+"""
+   Implements Python interface to CALIPSO Level-2 data.
+   By SG, Started Sep/2024
+   Files to be read are of the format as  
+          CAL_LID_L2_05kmALay-Standard-V4-51.YYYY-MM-DDTHH-MM-SSZD.hdf
+          
+   Main differences are related to the call "self._readList(Path)" (in def __init__ section) and 
+   related subcalls within it. 
+   
+   Notable differences with previous calipso_l2.py
+         1) it assumes the user provides a full path to a directory with many files or a single filename.  
+         2) Only ingests Day time orbit by checking the Day/Night flag for a pixel somwhere in the middles of array)
+            (this is can be removed since I usually provide a list of daytime files already prepared by the 
+            wrapper calling calipso_l2.py)
+         3) When reading  severeal orbits in a folder , the time ouput array is
+            separated by date datetime(2001,1,1,1,1,1) to make it easy to separate each orbit   
+         4) Minor additions but of relevance:
+           4.1 Created a new output array self.NOBS : number of orbits read    
+           4.2 Different number of variables to read , see ALIAS below
+         
+"""
+
+import os,sys
+from pyhdf import SD,HDF,VS
+from glob     import glob
+from   numpy    import ones, concatenate, array,linspace,arange,array,flip
+from   datetime import date, datetime, timedelta
+MISSING = -9999.0
+
+
+
+ALIAS = dict (
+                                                           Latitude  = 'lat' ,
+                                                          Longitude  = 'lon' ,
+                                                   Profile_UTC_Time  = 'Time',
+                                               Lidar_Data_Altitudes  = 'zlidar',
+                                  Total_Backscatter_Coefficient_532  = 'tback',
+                                         Extinction_Coefficient_532  = 'ext',
+                                         Extinction_Coefficient_1064 = 'ext2')
+
+SDS = list(ALIAS.keys())
+# Note: about CALIPSO latitude/long: 
+# Geodetic latitude, in degrees, of the laser footprint. For the 5 km profile products, three 
+# values are reported: the footprint latitude for the first pulse included in the 15 
+# shot average; the footprint latitude at the temporal midpoint 
+# (i.e., at the 8th of 15 consecutive laser shots); and the footprint latitude for 
+# the final pulse.
+#.........................................................................................
+
+class CALIPSO_L2(object):
+   """
+    Base class for generic CALIPSO object.
+   """
+
+   def __init__ (self,Path,keep=None,Verbose=0,only_good=True):
+     """
+       Creates an CALIPSO object defining the attributes corresponding
+       to the SDS's on input.
+        The optional parameter *keep* is used to specify the number of scan
+        lines (from the left of the swath) to keep. This is needed for
+        coping with the row anomaly problem.
+
+     """
+
+     #  Initially are lists of numpy arrays for each granule
+     # ----------------------------------------------------
+     self.verb = 'no'  #yes
+     # #self.keep = keep
+     self.SDS = SDS
+     self.NOBS = [] # save number of observations in each orbit
+     # print("hello in calipso_lev2.py")
+
+     # Variable names
+     # --------------
+     self.Names = []
+
+     for name in SDS:
+             self.Names.append(name)
+     self.Names += ['nymd','nhms']
+
+     # Create empty lists for SDS to be read from orbit file;
+     #  each element of the list contains data for one orbit
+     # ------------------------------------------------------
+
+     for name in self.Names:
+         self.__dict__[name] = []
+     self.time = [] # to hold datetime objects
+     self.filelist=[] # initialize structure that will hold filenames read
+     
+
+     # Read each orbit, appending them to the list
+     # -------------------------------------------
+     
+     if type(Path) is list:
+        if len(Path) == 0:
+            self.nobs = 0
+            print("WARNING: Empty CALIPSO object created")
+            return
+     else:
+         Path = [Path, ]
+
+     self._readList(Path)
+     
+     # Make each attribute a single numpy array
+     # ----------------------------------------
+     for name in self.Names:
+            try:
+                self.__dict__[name] = concatenate(self.__dict__[name])
+                
+            except:
+                print("Failed concatenating "+name)
+     
+     # Determine index of "good" observations
+     # --------------------------------------
+     pass # to do
+     # Keep only "good" observations
+     # -----------------------------
+     if only_good:
+         pass
+ 
+     # Make aliases for compatibility with older code
+     # ----------------------------------------------
+#    Alias = ALIAS.keys()
+#      print("\n Done reading all files \n")
+     for name in self.Names:
+         if self.verb=='yes': print('   shape', name,array(self.__dict__[name]).shape)
+         if name in SDS:
+             self.__dict__[ALIAS[name]] = self.__dict__[name]
+     
+     # ODS friendly attributes
+     # -----------------------
+     pass # todo
+     
+     
+#---
+   def _readList(self,List):
+        """
+        Recursively, look for files in list; list items can
+        be files or directories.
+        """
+        for item in List:
+            if os.path.isdir(item):      self._readDir(item)
+            elif os.path.isfile(item):   self._readOrbit(item)
+            else:
+                print("%s is not a valid file or directory, ignoring it"%item)
+
+
+#---
+   def _readDir(self,dir):
+#         """Recursively, look for files in directory."""
+#         #### Read file list from directory, then select only those in Daytime (*D.hdf)
+#         #### and then sort the list in descending order from oldest to newest orbit. 
+#         items=os.listdir(dir)
+#         # Filter filenames to include only those ending in 'D.hdf' and sort them by date
+# #         breakpoint()
+#         new_list = sorted(
+#             [item for item in items if item.endswith('D.hdf')],
+#             key=lambda filename: datetime.strptime(
+#                 filename.split('.')[1].split('Z')[0], '%Y-%m-%dT%H-%M-%S'
+#             )
+#         )
+#         print("Hello !")
+        for item in dir:
+            path = dir + os.sep + item
+            # breakpoint()  # Add a breakpoint here
+            if os.path.isdir(path):      
+               # breakpoint()  # Add a breakpoint here
+               self._readDir(path)
+            elif os.path.isfile(path):   
+               # breakpoint()  # Add a breakpoint here
+               self._readOrbit(path)
+            else:
+                print("%s is not a valid file or directory, ignoring it"%item)
+                sys.exit()
+
+#---
+   def _readOrbit(self,filename):
+        """Reads one CALIPSO orbit with Level 2.0 data."""
+
+        self.filelist.append(filename.split('/')[-1])
+        # Reference time
+        # --------------
+        REF_DATE = datetime(1993,1,1,0,0,0)
+
+        # Open the CALIPSO file and loop over the datasets,
+        # extracting GEOLOCATION and Data fields
+        # ----------------------------------------------
+
+        if self.verb=='yes': print(" File <%s>"%filename)
+
+        f = SD.SD(filename)
+        #### Get Day_night SDS and Check if it is nightime in the central profile.
+        #### if it is nightime, then assume that the full profile is nightime and skip
+        sd = f.select('Day_Night_Flag') # Range Values: 0 = day; 1 = night
+        dn_flag=sd.get()
+        # breakpoint()  # Add a breakpoint here
+        i0= int(len(dn_flag[:,0])/2)
+        # print(' int(len(dn_flag[:,0])/2) =', i0)
+        if dn_flag[i0,0] == 1 :
+                print('         Select orbit is in Nighttime. Skip!')
+        else:
+           for name in self.SDS:
+             v = name
+             if self.verb=='yes': print('   v dataset', v)
+   #      ------------------------------------------------------------------
+             if v == 'Profile_UTC_Time':
+               sd = f.select(v)
+               Time  = sd.get()
+               nobs  = len(Time)
+               nymd  = ones(nobs).astype('int')
+               nhms  = ones(nobs).astype('int')
+               ###NOTE  append adds its argument as a single item, while extend 
+               ###     adds the elements of its argument one by one. 
+               ## one adds [4,5] append results as Output: [1, 2, 3, [4, 5]] and 
+               ##                        extend as Output: [1, 2, 3, 4, 5]
+               # self.__dict__[v].append(Time)       # time as on file
+               self.__dict__[v].extend(Time)       # time as on file
+               for i in range(nobs):
+                   yymmdd = Time[i,1]   # 3 times are reported, choose the middle
+                   nymd0 = int(Time[i,1])               
+                   nd    = Time[i,1] - nymd0
+                   nd0   = nd * 24.0
+                   hh    = int(nd0)
+                   nd1   = nd0 - hh
+                   nd2   = nd1 * 60
+                   mm    = int(nd2)
+                   nd3   = nd2 - mm
+                   nd4   = nd3 * 60
+                   ss    = int(nd4)
+                                   
+                   nymd[i]  = 20000000 + nymd0
+                   nhms[i]  = ((hh * 100) + mm) * 100 + ss
+                   
+                   self.nymd.append(nymd)
+                   self.nhms.append(nhms)
+                   
+                   year = int(nymd[i]/10000)
+                   month = int((nymd[i] - 10000*year)/100)
+                   day = nymd[i] - (year*10000 + month * 100) 
+                   self.time.append(datetime(year,month,day,hh,mm,ss))
+               self.NOBS.append(nobs)
+               # self.time.append(datetime(2001,1,1,1,1,1)) # add seprating date that will be used to separate each orbit
+             elif v == 'Lidar_Data_Altitudes':
+                  # alitude is not a dataset, it is stored in a VD structured
+                  # the following lines  are from https://forum.earthdata.nasa.gov/viewtopic.php?t=4222
+                  #it works. but it seems that it can be improved.
+                  #get altitude
+                  hdf = HDF.HDF(filename)
+                  vs = hdf.vstart() ; xid = vs.find('metadata'); altid = vs.attach(xid)
+                  altid.setfields('Lidar_Data_Altitudes')
+                  nrecs, _, _, _, _ = altid.inquire()
+                  altitude = altid.read(nRec=nrecs)
+                  altid.detach()
+#                   alti = np.array(altitude[0][0])
+#                   data = np.flip(alti)
+                  alti = array(altitude[0][0])
+                  data = flip(alti)
+                  self.__dict__[v].append(data)               
+             else:
+                 
+                 sd    =  f.select(v)
+                 data  = sd.get()  # most of parameters : data = (nobs) or (nobs,km) except L2 feature type(nobs,km,4)
+                 if v == 'Temperature': data = data + 273.15
+                 # for aerosol arrays: 1st dim: same as time, 2nd dim: Nlayer=399
+                 # for lat/lon arrays: 1st dim: same as time, 2nd dim: 3
+                 self.__dict__[v].append(data[:,:]) 
+          
+#---
+   def writeg(self,aer,syn_time,nsyn=8,filename=None,dir='.',expid='calipso_lev2',Verb=1):
+       """
+        Writes gridded CALIPSO measurements to file (same grid as GEOS-5 file).
+        Verb -- Verbose level:
+                 0 - really quiet (default)
+                 1 - Warns if invalid file is found
+                 2 - Prints out non-zero number of fires in each file.
+
+       """
+       from gfio     import GFIO
+       from binObs_  import binobs3d, binobs3dp
+
+       # Determine synoptic time range
+       # -----------------------------
+       dt = timedelta(seconds = 12. * 60. * 60. / nsyn)
+       t1, t2 = (syn_time-dt,syn_time+dt)
+      
+#      Lat lon grid from GEOS-5 file
+#      ------------
+       im = aer.im
+       jm = aer.jm
+      
+       glon = linspace(-180.,180.,im,endpoint=False)
+       glat = linspace(-90.,90.,jm)
+      
+       nymd = 10000 * syn_time.year + 100 * syn_time.month  + syn_time.day
+       nhms = 10000 * syn_time.hour + 100 * syn_time.minute + syn_time.second
+
+       if self.verb=='yes': print('in writeg nymd=',nymd, 'nhms=',nhms) 
+       km = aer.km             # vertical levels
+       ptop = 1.               # ~ 1 Pa at the top
+
+#      GEOS-5 edge pressure [Pa]
+#      ------------------------- 
+       pe = ones((im,jm,km+1)) 
+       pe[:,:,0] = ptop
+       for k in range(aer.km):
+           pe[:,:,k+1] = pe[:,:,k] + aer.read('delp',nymd=nymd,nhms=nhms)[:,:,k]   
+       pe = pe * 0.01         # in hPa
+
+#      GEOS-5 mid-level pressure [Pa]
+#      -----------------------------
+       plev = ones((im,jm,km)) # mid-level pressure [Pa]
+       plev[:,:,0] = ptop + aer.read('delp',nymd=nymd,nhms=nhms)[:,:,0]/2. 
+       for k in range(aer.km-1):  
+           plev[:,:,k+1] = plev[:,:,k] + aer.read('delp',nymd=nymd,nhms=nhms)[:,:,k]/2.\
+           + aer.read('delp',nymd=nymd,nhms=nhms)[:,:,k+1]/2.      
+       plev = plev * 0.01
+       
+       vtitle = [ 'tback',
+                  'tback_err',
+                  'extinction', 
+                  'ext_err',
+#                  'mol_aback',
+                  'pressure' ]
+
+       vname  = ['tback','tback_err', 'ext', 'ext_err', 'pressure' ]
+       vunits = [ 'km-1 sr-1','km-1 sr-1',  'km-1',   'km-1', 'hPa' ]
+       kmvar  = [km,      km,    km,    km,  km ]
+
+       title = 'Gridded CALIPSO Level 2 version 3.01 data'
+       source = 'NASA/GSFC/GMAO GEOS-5 Aerosol Group'
+       contact = 'arlindo.dasilva@nasa.gov'
+
+       if filename is None:
+           filename = '%s/%s.obs_l3a.%d_%02dz.nc4'%(dir,expid,nymd,nhms/10000)
+       
+#      Create the file
+#      ---------------
+       f = GFIO()
+       glevs=arange(km)
+       f.create(filename, vname, nymd, nhms,
+                lon=glon, lat=glat, levs=glevs, levunits='hPa',
+                vtitle=vtitle, vunits=vunits,kmvar=kmvar,amiss=MISSING,
+                title=title, source=source, contact=contact)
+
+       # QA filtering
+       # ------------
+       I_bad = ones(self.tback.shape) # bad data
+       I_bad = False
+       
+       # Time filter of data
+       # -------------------
+#       lon = self.lon.ravel()
+#       lat = self.lat.ravel()
+
+       lon = self.lon[:,1]  # choose the middle pulse
+       lat = self.lat[:,1]
+
+
+       tback = _timefilter(self.time,t1,t2,self.tback,I_bad)
+       tback_err = _timefilter(self.time,t1,t2,self.tback_err,I_bad)
+       ext = _timefilter(self.time,t1,t2,self.ext,I_bad)
+       ext_err = _timefilter(self.time,t1,t2,self.ext_err,I_bad)
+       pressure = _timefilter(self.time,t1,t2,self.plev,I_bad)
+       
+       gObs=binobs3dp(lon,lat,pressure,tback,pe,MISSING)
+       
+#      Grid variable and write to file
+#      -------------------------------
+       f.write('tback', nymd, nhms, binobs3dp(lon,lat,tback,pressure,pe,MISSING) )
+       f.write('tback_err', nymd, nhms, binobs3dp(lon,lat,tback_err,pressure,pe,MISSING) )
+       f.write('ext',    nymd, nhms, binobs3dp(lon,lat,ext,pressure,pe,MISSING) )
+       f.write('ext_err', nymd, nhms, binobs3dp(lon,lat,ext_err,pressure,pe,MISSING) )  
+       f.write('pressure', nymd, nhms, plev)
+
+       if Verb >=1:
+           print("[w] Wrote file "+filename)
+           
+#....................................................................
+
+def _timefilter ( t, t1, t2, a, I_bad ):
+    filler = MISSING * ones(a.shape[1:])
+    b = a.copy()
+    for i in range(len(t)):
+        if (t[i]<t1) or (t[i]>=t2):
+            b[i] = filler
+    if len(b.shape) == 3:
+        b[I_bad,:] = MISSING
+    elif len(b.shape) == 2:
+        b[I_bad] = MISSING
+    else:
+        raise IndexError("Invalid rank=%d for time filtering"%len(b.shape))
+    return b
+#---
+def orbits (path, syn_time, nsyn=8, Verbose=0 ):
+    """
+    Returns a list of CALIPSO orbits for a given product at given synoptic time.
+    On input,
+
+    path      ---  mounting point for the CALIPSO Level 2 files
+    syn_time  ---  synoptic time (timedate format)
+
+    nsyn      ---  number of synoptic times per day (optional)
+
+    """
+
+    # Determine synoptic time range
+    # -----------------------------
+    dt = timedelta(seconds = 12. * 60. * 60. / nsyn)
+    t1, t2 = (syn_time-dt,syn_time+dt)
+
+    print("[*] ", t1,"|", t2)
+
+    today = syn_time
+    yesterday = today - timedelta(hours=24)
+
+    Files = []
+    for t in (yesterday,today):
+
+        yy, mm, dd = (t.year,t.month,t.day)        
+       
+        dirn = "%s/"%(path)        
+        Files += glob("%s/CAL_LID_L2_05kmCPro-Prov-V3-01.*.hdf"%(dirn))
+        
+    Orbits = []
+    for f in Files:
+        dirn, filen = os.path.split(f)
+        
+        tokens = filen.split('-')
+        beg_yy = int(tokens[3].split('.')[1])
+        beg_mm = int(tokens[4])
+        beg_dd = int(tokens[5].split('T')[0])
+        beg_h = int(tokens[5].split('T')[1])
+        beg_m = int(tokens[6])
+        t_beg = datetime(beg_yy,beg_mm,beg_dd,beg_h,beg_m,0)
+        t_end = t_beg + timedelta(minutes=90)
+        # t_end = datetime(end_yy,end_mm,end_dd,end_h,end_m,0)
+        
+        if (t_beg>=t1 and t_beg<t2) or (t_end>=t1 and t_end<t2):
+            print("[x] ", t_beg, '|', t_end)
+            Orbits += [f,]
+            
+            if Verbose:
+                print("[] ", f)
+
+    return Orbits
+#............................................................................
+
+if __name__ == "__main__":
+
+#    syn_time = datetime(2008,6,30,0,0,0)
+    syn_time = datetime(2011,6,1,9,0,0)
+#    Files = orbits('/nobackup/CALIPSO/Level1.5',syn_time,Verbose=1)
+    
+#    essai  = CALIPSO_L1p5(Files,Verbose=1)
+    
+#    essai.writeg(syn_time=syn_time,nsyn=8,filename=None,\
+#    dir='/nobackup/2/vbuchard/',expid='calipso_lev2',refine=8,res=None,Verb=1)
