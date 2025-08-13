1fa4f146fd2136d5ea5f7f8ccd36373628e10ef3
Patricia Castellanos <patricia.castellanos@nasa.gov>
2025-08-13 11:12:48 -0400

    fix typo

--- src/pyobs/vx04.py
+++ src/pyobs/vx04.py
@@ -319,7 +319,7 @@ class Vx04_L2(object):
                self.iGood = self.iGood & ~self.solar_azimuth_angle.mask
        elif self.algo == 'DT_OCEAN':
            self.iGood = (self.Land_Ocean_Quality_Flag > BAD) & (~self.Effective_Optical_Depth_Average_Ocean.mask[:,1])
-           if type(self.ScatteringAngle.mask) is not np.bool:
+           if type(self.Scattering_Angle.mask) is not np.bool:
                self.iGood = self.iGood & ~self.Scattering_Angle.mask
                self.iGood = self.iGood & ~self.sensor_azimuth_angle.mask
                self.iGood = self.iGood & ~self.solar_azimuth_angle.mask               
